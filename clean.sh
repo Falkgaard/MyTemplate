28582fd3f4afa9f655b76bf1fa574cb16b26867d
Falkgaard <victor.itzel@gmail.com>
2021-11-28 14:28:48 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-printf '\033[0;1;37mCleaning project...\033[0m\n'
+printf '\033[0;1;37mCleaning project...\033[0m'
 rm -fr ./build/
 rm -fr ./debug/
 rm -fr ./.cache/
 rm -fr ./bin/
-printf '\033[0;1;37m... done!\033[0m\n'
+printf '\033[0;1;37m done!\033[0m\n'
