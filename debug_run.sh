6c35083d311db79cda592b4db19138c5e45e3c85
Falkgaard <victor.itzel@gmail.com>
2021-11-28 14:28:06 +0100

    Update debug_run.sh

--- debug_run.sh
+++ debug_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-printf '\n \033[0;1;37mRunning debug build...\033[0m\n'
+printf '\033[0;1;37mRunning debug build...\033[0m\n'
 cd ./debug/
 ./MyTemplate
-printf '\n \033[0;1;37... done!\033[0m\n'
+printf '\033[0;1;37m... done!\033[0m\n'
