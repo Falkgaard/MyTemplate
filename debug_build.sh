7776ab8dffd26e0d0e376754e139267ea918300f
Falkgaard <victor.itzel@gmail.com>
2021-11-28 14:27:46 +0100

    Update debug_build.sh

--- debug_build.sh
+++ debug_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-printf '\n \033[0;1;37mBuilding debug build...\033[0m\n'
+printf '\033[0;1;37mBuilding debug build...\033[0m\n'
 cmake -S . -Bdebug -DCMAKE_BUILD_TYPE=debug && cd ./debug/ && make
-printf '\n \033[0;1;37... done!\033[0m\n'
+printf '\033[0;1;37m... done!\033[0m\n'
